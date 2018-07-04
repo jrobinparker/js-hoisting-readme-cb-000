8ae94e39d2771923cbec9ef3295d57e13f981135
Jeremy Parker <jrobinparker@gmail.com>
2018-07-04 01:40:32 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -19,9 +19,9 @@ function crazy() {
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
-    function sayMy() {
+  function sayMy() {
     console.log(name);
     var name = "Kristin";
-  }
+  };
   sayMy();
 }
